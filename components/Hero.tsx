98465d79ad99f2724a79f534a5b6bf853e954276
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-16 17:05:44 +0200

    changed variant to string

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -43,7 +43,7 @@ const Hero = () => {
     </p>
     </div>
 
-    <div className="flex w-full gap-3 sm:flex-row">
+    <div className="flex flex-col w-full gap-3 sm:flex-row">
       <Button type="button" title="Download App" variant="btn_green" />
       <Button type="button" title="How We Work?" icon="/play.svg" variant="btn_white_text" />
     </div>
