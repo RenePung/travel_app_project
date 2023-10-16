10f78d83f6fe364e10849e2d2ddc2bdd03030a25
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-16 11:27:08 +0200

    added button styling & bux fix
    bug = backticks should be used not normal single ticks

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
 import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
+import Button from "./Button"
 
 const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
-      
+
     <Link href="/">
     <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
     </Link>
@@ -20,6 +21,16 @@ const Navbar = () => {
       </Link>
     ))}
     </ul>
+
+
+    <div className="lg:flexCenter hidden">
+    <Button 
+    type="button"
+    title="Login"
+    icon="/user.svg"
+    variant="btn_dark_green"
+    />
+    </div>
     </nav>
   )
 }
