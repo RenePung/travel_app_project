c21db3f89d24d379c3f9d38b11ebb632e6ea83ec
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-17 09:42:48 +0200

    added text

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -6,6 +6,13 @@ const Guide = () => {
     <section className="flexCenter flex-col">
       <div className="border-2 border-red-500 padding-container max-container w-full pb-24">
         <Image src="/camp.svg" alt="camp" width={50} height={50} />
+        <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+          We Are Here For You
+        </p>
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+          <h2>Guide You To Easy Path</h2>
+          <p>Only with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
+        </div>
       </div>
     </section>
   )
