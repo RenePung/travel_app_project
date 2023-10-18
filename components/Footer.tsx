3318fc82254cec9dc99eea81cca37c67695b9aff
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-18 10:25:02 +0200

    added contacts

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,12 @@
-import { FOOTER_LINKS } from '@/constants'
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+
+
+
+
+
 
 const Footer = () => {
   return (
@@ -24,12 +29,44 @@ const Footer = () => {
                 </ul>
               </FooterColumn>
             ))}
+
+
+
+
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+                {FOOTER_CONTACT_INFO.links.map((link) => (
+                  <Link href="/" key={link.label} className="flex gap-4 md:flex-col lg:flex-row">
+                    <p className="whitespace-nowrap">{link.label}:</p>
+                    <p className="medium-14 whitespace-nowrap text-blue-17">{link.value}</p>
+                  </Link>
+                ))}
+              </FooterColumn>
+            </div>
+
+
+            
           </div>
         </div>
       </div>
     </footer>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 type FooterColumnProps = {
   title: string;
