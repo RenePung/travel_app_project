46f995285d6dda67ec4ab4ba20019c8d82b0d77d
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-18 09:05:31 +0200

    added buttons & fixed bug & added text
    hover over buttons was not full

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Button from './Button'
 
 const GetApp = () => {
   return (
     <section className="flexCenter w-full flex-col pb-[100px]">
       <div className="get-app">
-
+        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get For Free Now!</h2>
+          <p className="regular-16 text-gray-10">Available On IOS And Android, Download Now!</p>
+          <div className="">
+            <Button type="button" title="App Store" icon="/apple.svg" variant="btn_white" full />
+            <Button type="button" title="Google Play" icon="/android.svg" variant="btn_dark_green_outline" full />
+          </div>
+        </div>
       </div>
     </section>
   )
