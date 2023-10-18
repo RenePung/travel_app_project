fa7a8f7a733da43c70c9e3b64f65a2bf6e845247
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-18 10:10:06 +0200

    rendering links - creating footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import { FOOTER_LINKS } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -11,8 +12,18 @@ const Footer = () => {
             <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
           </Link>
 
-          <div className="">
-
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+            {FOOTER_LINKS.map((columns) => (
+              <FooterColumn title={columns.title}>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {columns.links.map((link) => (
+                    <Link href="/" key={link}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            ))}
           </div>
         </div>
       </div>
@@ -20,4 +31,20 @@ const Footer = () => {
   )
 }
 
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+}
+
+
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">
+        {title}
+      </h4>
+      {children}
+    </div>
+  )
+}
 export default Footer
